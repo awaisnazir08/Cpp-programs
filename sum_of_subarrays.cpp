ea663d6d51c9cce82d03d7b39ec4557fd2c45adb
awaisnazir08 <owaisnazir2228@gmail.com>
2023-09-14 16:39:57 +0500

    sum of subarrays and longest arithmetic subarray

---
+++ sum_of_subarrays.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s = 0;
+    int arr[4] = {1,2,3,4};
+    for (int i=0;i<4;i++)
+    {
+        int temp=0;
+        for(int j=i;j<4;j++)
+        {
+            s+=arr[j];
+            temp=j-1;
+            while(temp>=i)
+            {
+                s+=arr[temp];
+                temp--;
+            }
+        }
+    }
+    cout<<s<<endl;
+    return 0;
+}
