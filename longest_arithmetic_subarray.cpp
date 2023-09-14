cb0ca00b322fb17b3dfc078a21ba9d2cb74e2204
awaisnazir08 <owaisnazir2228@gmail.com>
2023-09-14 16:40:23 +0500

    more

---
+++ longest_arithmetic_subarray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int max(int a, int b)
+{
+    if(a>b){
+        return a;
+    }
+    return b;
+}
+int main()
+{
+    int arr[10] = {2,4,6, 7, 10, 13, 16, 19, 22, 30};
+    int diff = arr[1]-arr[0];
+    int curr = 2;
+    int temp = 0;
+    int m = 0;
+    for (int i=1; i<9 ;i++)
+    {
+        
+        if(arr[i+1]-arr[i]==diff)
+        {
+            curr+=1;
+            m=max(temp,curr);
+        }
+        else{
+            temp = curr;
+            diff = arr[i+1]-arr[i];
+            curr = 2;
+            max(temp, curr);
+        }
+    }
+    cout<<m<<endl;
+    return 0;
+}
