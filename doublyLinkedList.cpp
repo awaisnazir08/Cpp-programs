3e0a2e585fe491529c7483f632e332d6ae2c1e74
awaisnazir08 <owaisnazir2228@gmail.com>
2023-09-27 18:50:54 +0500

    added some more functions to doubly linked list

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -14,6 +14,50 @@ public:
     }
 };
 
+void deleteAtHead(node *&list)
+{
+    if (list == NULL)
+    {
+        return;
+    }
+    else if ((list->next == NULL))
+    {
+        list = NULL;
+        return;
+    }
+    else
+    {
+        node *toDelete = list;
+        list->next->prev = NULL;
+        list = list->next;
+        delete toDelete;
+    }
+}
+
+void deleteAtTail(node *&list)
+{
+    if (list == NULL)
+    {
+        return;
+    }
+    else if (list->prev == NULL && list->next == NULL)
+    {
+        list = NULL;
+        return;
+    }
+    else
+    {
+        node *temp = list;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        node *toDelete = temp;
+        temp->prev->next = NULL;
+        delete toDelete;
+    }
+}
+
 void insertAtHead(node *&list, int val)
 {
     node *n = new node(val);
@@ -47,12 +91,92 @@ void insertAtTail(node *&list, int val)
 void displayNode(node *list)
 {
     node *temp = list;
+    cout << "NULL->>";
     while (temp != NULL)
     {
-        cout << temp->data << "->";
+        cout << temp->data << "->>";
         temp = temp->next;
     }
-    cout << "NULL";
+    cout << "NULL" << endl;
+}
+void displayReverse(node *list)
+{
+    if (list == NULL)
+    {
+        cout << "NULL<<-NULL" << endl;
+        return;
+    }
+    node *temp = list;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    cout << "NULL<<-";
+    while (temp != NULL)
+    {
+        cout << temp->data << "<<-";
+        temp = temp->prev;
+    }
+    cout << "NULL" << endl;
+}
+
+void deleteAtPosition(node *&list, int pos)
+{
+    try 
+    {
+        if(pos <= 0)
+        {
+            throw std::runtime_error("The position size must be greater than zero..!!");
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(0);
+    }
+    if (list == NULL)
+    {
+        return;
+    }
+    else if (pos == 1)
+    {
+        deleteAtHead(list);
+    }
+    else
+    {
+        node *temp = list;
+        int *counter = new int(1);
+        while (*counter != pos)
+        {
+            (*counter)++;
+            try
+            {
+                /* code */
+                temp = temp->next;
+                if (temp == NULL && *counter != pos)
+                {
+                    delete counter;
+                    throw std::runtime_error("The position that you have entered is out of bounds!");
+                }
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+        }
+        delete counter;
+        if (temp->next != NULL && temp->next != NULL)
+        {
+            node *toDelete = temp;
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            delete toDelete;
+        }
+        else if (temp->next == NULL)
+        {
+            deleteAtTail(list);
+        }
+    }
 }
 
 int main()
@@ -61,6 +185,10 @@ int main()
     insertAtTail(list, 43);
     insertAtHead(list, 2);
     insertAtHead(list, 4);
+    insertAtHead(list, 7);
+    displayNode(list);
+    deleteAtPosition(list, 4);
+    deleteAtPosition(list, 2);
     displayNode(list);
     return 0;
 }
