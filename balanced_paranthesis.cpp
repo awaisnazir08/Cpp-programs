7543472a2c96bb80de583a0a3a5aa92d94d2010d
awaisnazir08 <owaisnazir2228@gmail.com>
2023-11-07 15:16:40 +0500

    implemented stack using queues

--- balanced_paranthesis.cpp
+++ balanced_paranthesis.cpp
@@ -1,62 +1,67 @@
-#include<iostream>
-#include<stack>
-#include<string>
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 bool balancedParenthesis(string b)
 {
-    stack<char>s;
-    for(int i = 0; i<b.size(); i++)
+    stack<char> s;
+    for (int i = 0; i < b.size(); i++)
     {
-        if(s.empty())
-        {
-            s.push(b[i]);
-        }
-        else if(b[i]=='{' || b[i]=='[' || b[i]=='(')
+        if (b[i] == '{' || b[i] == '[' || b[i] == '(')
         {
             s.push(b[i]);
         }
         else
         {
-            switch (b[i])
+            if(b[i] == ')' || b[i]=='}' || b[i]==']')
             {
-                case ')':
-                if(s.top()=='(')
+                if(s.empty())
                 {
-                    s.pop();
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-                case '}':
-                if(s.top()=='{')
-                {
-                    s.pop();
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-                                case ']':
-                if(s.top()=='[')
-                {
-                    s.pop();
-                    break;
-                }
-                else{
                     return false;
                 }
             }
-
+            switch (b[i])
+            {
+            case ')':
+                if (s.top() == '(')
+                {
+                    s.pop();
+                    break;
+                }
+                else
+                {
+                    return false;
+                }
+            case '}':
+                if (s.top() == '{')
+                {
+                    s.pop();
+                    break;
+                }
+                else
+                {
+                    return false;
+                }
+            case ']':
+                if (s.top() == '[')
+                {
+                    s.pop();
+                    break;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
-    return true;
+    if (s.empty())
+        return true;
+    return false;
 }
 int main()
 {
     string s = "1 + 2 *[ 3 * 3 + { 4 - 5 ( 6 ( 7 / 8 / 9 ) + 10 ) } - 11 + ( 12 * 8) / {13 + 13}] + 14";
-    cout<<balancedParenthesis(s);
+    cout << balancedParenthesis(s);
     return 0;
 }
---
+++ stack_using_queue.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Stack
+{
+    queue<int> q;
+    queue<int> q2;
+
+public:
+    void push(int v)
+    {
+        q.push(v);
+    }
+    void pop()
+    {
+        if (q.empty())
+        {
+            cout << "The stack is empty!" << endl;
+        }
+        int size = q.size() - 1;
+        for (int i = 0; i < size; i++)
+        {
+            q2.push(q.front());
+            q.pop();
+        }
+        q.pop();
+        while (!q2.empty())
+        {
+            q.push(q2.front());
+            q2.pop();
+        }
+    }
+
+    int top()
+    {
+        if (!q.empty())
+            return q.back();
+        else
+            cout << "The stack is empty!" << endl;
+        return -1;
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
+int main()
+{
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+
+    return 0;
+}
