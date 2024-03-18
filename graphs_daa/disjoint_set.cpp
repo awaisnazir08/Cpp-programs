06ef86feddf7d83044dc671e2a110a8400091b31
awaisnazir08 <owaisnazir2228@gmail.com>
2024-03-18 23:24:01 +0500

    union by size implemented in disjoint sets

--- graphs_daa/disjoint_set.cpp
+++ graphs_daa/disjoint_set.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
     Public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1);
         for(int i = 0; i <=n; i++)
         {
             parent[i] = i;
+            size[i] = 1;
         }
 
     }
@@ -44,6 +46,26 @@ class DisjointSet
             //can do the other way round as well
             parent[ulp_u] = ulp_v;
             rank[ulp_V]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if (ulp_u == ulp_v)
+        {
+            return;
+        }
+        if(size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
